3045be1e6919fc53ce3da8a6687dfc81e1c614a0
Gabriel Stoppa Dos Santos <gabrielstoppadossantos@gmail.com>
2024-04-28 13:11:38 -0300

    Add new icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
-import embeds from 'astro-embed/integration';
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
