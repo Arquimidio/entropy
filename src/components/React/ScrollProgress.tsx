fe7af6789c4b006f5a56701cead2fa9084b6fc20
Gabriel Stoppa Dos Santos <gabrielstoppadossantos@gmail.com>
2024-04-28 13:45:15 -0300

    Add scroll progress bar

---
+++ src/components/React/ScrollProgress.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+
+export default function ScrollProgress() {
+    const [scrollProgress, setScrollProgress] = useState(0);
+
+    const trackScroll = () => {
+        const scrollTop = window.scrollY;
+        const scrollHeight = document.body.scrollHeight - document.body.clientHeight;
+        const scrollPercentRounded = Math.round(scrollTop / scrollHeight * 100);
+
+        setScrollProgress(scrollPercentRounded);
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', trackScroll);
+
+        return () => {
+            window.removeEventListener('scroll', trackScroll);
+        }
+    }, [])
+
+    return (
+        <div className="fixed top-0 left-0 w-[100vw] h-[2px]">
+            <div className="h-full bg-rose-500" style={{
+                width: `${scrollProgress}%`
+            }}></div>
+        </div>
+    )
+}
